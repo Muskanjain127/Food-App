8bd503c2d16d086685fba8582fbf7d5481ae0ff5
Muskanjain127 <146107376+Muskanjain127@users.noreply.github.com>
2026-06-26 18:22:02 +0530

    Update usercontroller.js

--- backend/controller/usercontroller.js
+++ backend/controller/usercontroller.js
@@ -32,7 +32,7 @@ async function userregister(req, res) {
     const token = jwt.sign({ id: user._id }, process.env.JWTSECRET);
 res.cookie("userlogintoken", token, {
   httpOnly: true,
-  Secure: true,       
+  secure: true,       
   sameSite: "none",   
   path: "/",
   maxAge:36000000
@@ -67,7 +67,7 @@ async function userlogin(req, res) {
     }
 
     const token = jwt.sign({ id: user._id }, process.env.JWTSECRET);
-    res.cookie("userlogintoken", token, { httpOnly: true, sameSite: "none" });
+    res.cookie("userlogintoken", token, { httpOnly: true, sameSite: "none",secure:true });
 
     res.status(200).json({
       message: "User logged in successfully",
