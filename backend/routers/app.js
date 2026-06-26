ce4a5fcac2e359af0877ab95ec25b323c27015ce
Muskanjain127 <146107376+Muskanjain127@users.noreply.github.com>
2026-06-26 18:08:03 +0530

    Update app.js

--- backend/routers/app.js
+++ backend/routers/app.js
@@ -11,6 +11,8 @@ app.use(
   cors({
     origin: ["https://food-app-one-azure.vercel.app", "http://localhost:5173"],
     credentials: true,
+    methods: ["GET", "POST", "PUT", "DELETE"],
+  allowedHeaders: ["Content-Type", "Authorization"]
   }),
 );
 const userrouter = require("../routers/userrouter");
