6ae2358e1a7391f38046a7500564011eb78f3012
Muskanjain127 <146107376+Muskanjain127@users.noreply.github.com>
2026-06-26 18:42:03 +0530

    Update foodlist.js

--- backend/controller/foodlist.js
+++ backend/controller/foodlist.js
@@ -1,7 +1,9 @@
 const fooditem = require("../model/fooditemmodel");
 const foodpartnermodel = require("../model/foodpartnermodel");
 async function showfooditems(req, res) {
+  console.log("in showfooditems");
   const items = await fooditem.find({}).populate("foodpartner");
+  console.log("items",items);
   res.send(items,req.user);
 }
 module.exports = showfooditems;
